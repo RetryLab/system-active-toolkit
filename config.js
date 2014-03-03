96c1a299c616e07f6b93cf3c05d916dc4fd30665
sun <241456911@qq.com>
2014-03-03 21:35:42 +0800

    update load traffic

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ var isProduction = process.env.NODE_ENV === 'production' ? true : false;
 module.exports = {
 	'FILE_NET_SNMP': isProduction ? '/proc/net/snmp' : __dirname + '/test/snmp',
 	'FILE_STAT': isProduction ? '/proc/stat' : __dirname + '/test/stat',
-	'FILE_MEMINFO': isProduction ? '/proc/meminfo' : __dirname + '/test/meminfo'
+	'FILE_MEMINFO': isProduction ? '/proc/meminfo' : __dirname + '/test/meminfo',
+	'FILE_LOADAVG': isProduction? '/proc/loadavg' : ___dirname + '/test/loadavg',
+	'FILE_TRAFFIC': isProduction? '/proc/net/dev' : ___dirname + '/test/dev'
 }
