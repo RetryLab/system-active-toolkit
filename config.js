a6c523453f9b2e1efce34b28bf826ce23d62da62
root <root@debian7>
2014-03-04 15:47:51 +0800

    update trffic

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-var isProduction = process.env.NODE_ENV === 'production' ? true : false;
 module.exports = {
-	'FILE_NET_SNMP': isProduction ? '/proc/net/snmp' : __dirname + '/test/snmp',
-	'FILE_STAT': isProduction ? '/proc/stat' : __dirname + '/test/stat',
-	'FILE_MEMINFO': isProduction ? '/proc/meminfo' : __dirname + '/test/meminfo',
-	'FILE_LOADAVG': isProduction? '/proc/loadavg' : ___dirname + '/test/loadavg',
-	'FILE_TRAFFIC': isProduction? '/proc/net/dev' : ___dirname + '/test/dev'
+	'FILE_NET_SNMP': '/proc/net/snmp',
+	'FILE_STAT': '/proc/stat',
+	'FILE_DISK_STATS': '/proc/diskstats',
+	'FILE_MEMINFO': '/proc/meminfo',
+	'FILE_LOADAVG': '/proc/loadavg',
+	'FILE_NET_DEV': '/proc/net/dev'
 }
