cdcc9fd42686f70392f6ab1a2e007d77e0b963bb
Tad Wang <wptad@tom.com>
2014-03-05 17:12:30 +0800

    ad tcpx

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	'FILE_NET_SNMP': '/proc/net/snmp',
+	'FILE_NET_TCP': '/proc/net/tcp',
+	'FILE_NET_NETSTAT': '/proc/net/netstat',
 	'FILE_STAT': '/proc/stat',
 	'FILE_DISK_STATS': '/proc/diskstats',
 	'FILE_MEMINFO': '/proc/meminfo',
